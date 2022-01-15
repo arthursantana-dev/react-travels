99b3a4f170b76dc2ef4dcbd427a76f695e19a711
Arthur Santana <arthur.alexandre330@gmail.com>
2022-01-15 20:47:53 -0300

    pequenos ajustes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import Header from './components/Header';
-import Card from './components/Card'
+import "./App.css";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-		<Header />
-		<section className="card__list">
-			<Card />
-		</section>
+      <Header />
+      <section className="card__list">
+        {data.map((e) => {
+          return <Card key={e.id} item={e} />;
+        })}
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		id: 0,
+        title: "Monte Fuji",
+        location: "Japão",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "O Monte Fuji é a montanha mais alta do Japão, com 3.776 metros (12.380 pés). O Monte Fuji é o local turístico mais popular do Japão, tanto para turistas japoneses quanto estrangeiros.",
+        imageUrl: "https://images.unsplash.com/photo-1589308078059-be1415eab4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+	{
+		id: 2,
+		title:'Casa de Ópera de Sydney',
+		location:'Austrália',
+		googleMapsUrl: 'https://goo.gl/maps/kuZMri7LwBwuNM3k7',
+		startDate:'27 May, 2021',
+		endDate:'8 Jun, 2021',
+		description:'A Sydney Opera House é um centro de artes cênicas com vários locais em Sydney. Localizado às margens do porto de Sydney, é frequentemente considerado um dos edifícios mais famosos e distintos do século XX',
+		imageUrl:'https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80'
+	},
+	{
+	   id: 3,
+	   title:'Geirangerfjord',
+	   location:'Noruega',
+	   googleMapsUrl: 'https://goo.gl/maps/qDnDHQgSptsQnuFK9',
+	   startDate:'01 Out, 2021',
+	   endDate:'18 Nov, 2021',
+	   description:'O Geiranger Fjord é um fiorde na região de Sunnmøre do condado de Møre og Romsdal, Noruega. Está localizado inteiramente no município de Stranda.',
+	   imageUrl:'https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+   }
+]
